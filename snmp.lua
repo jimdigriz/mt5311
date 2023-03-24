e82a92bf5a11212204ac61f0a7a144df868c1cb0
Alexander Clouter <alex@digriz.org.uk>
2023-03-24 01:29:26 +0000

    guards

--- snmp.lua
+++ snmp.lua
@@ -4,8 +4,8 @@
 local socket = require "posix.sys.socket"
 if socket.AF_PACKET == nil then error("AF_PACKET not available, did you install lua-posix 35.1 or later?") end
 -- https://github.com/iryont/lua-struct
-local struct = pcall(function() return require "struct" end)
-if not struct then
+local status, struct = pcall(function () return require "struct" end)
+if not status then
 	struct = assert(loadfile(arg[0]:match("^(.-/?)[^/]+.lua$") .. "struct.lua"))
 end
 
@@ -46,6 +46,10 @@ end
 local iface = arg[1]
 -- luaposix does not support ioctl(fd, SIOCGIFHWADDR, &s))
 local iface_macaddr_f = io.open("/sys/class/net/" .. iface .. "/address")
+if not iface_macaddr_f then
+	io.stderr:write("invalid IFACE\n")
+	os.exit(1)
+end
 local iface_macaddr = macaddr2bytes(iface_macaddr_f:read())
 iface_macaddr_f:close()
 
@@ -58,6 +62,9 @@ local little_endian = string.dump(function() end):byte(7) == 1
 -- luaposix does not support AF_PACKET/SOCK_DGRAM :(
 local fd = assert(socket.socket(socket.AF_PACKET, socket.SOCK_RAW, little_endian and 0x2061 or 0x6120))
 assert(socket.bind(fd, {family=socket.AF_PACKET, ifindex=socket.if_nametoindex(iface)}))
+-- pkt = socket.recv(fd, 1000)
+-- remember to filter on dst (our) macaddr as someone may have the NIC set to promisc
+-- print(pkt)
 pkt = macaddr .. iface_macaddr .. proto .. "1111"
 assert(socket.send(fd, pkt) == pkt:len())
 require "posix.unistd".close(fd)
