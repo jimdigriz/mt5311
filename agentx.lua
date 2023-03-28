78097ca4b6d6569f317953ab612f5a10d9fe56d7
Alexander Clouter <alex@digriz.org.uk>
2023-03-28 17:43:46 +0100

    wip

--- agentx.lua
+++ agentx.lua
@@ -17,7 +17,6 @@ end
 
 local unpack = table.unpack or _G.unpack
 
-local DEADTIME = 3
 local HDRSIZE = 20
 
 local FLAGS = {
@@ -28,31 +27,35 @@ local FLAGS = {
 	NETWORK_BYTE_ORDER	= 0x10
 }
 
-local TYPE = {
-	_hdr		= 0,
-	open		= 1,
-	close		= 2,
-	indexAllocate	= 14,
-	indexDeallocate	= 15,
-	response	= 18
+local PTYPE = {
+	_hdr			= 0,
+	open			= 1,
+	close			= 2,
+	register		= 3,
+	unregister		= 4,
+	indexAllocate		= 14,
+	indexDeallocate		= 15,
+	response		= 18
 }
 
 local VTYPE = {
-	integer		= 2,
-	octetstring	= 4
+	integer			= 2,
+	octetstring		= 4,
+	objectid		= 6
 }
 
 local ERROR = {
-	noAgentXError	= 0
+	noAgentXError		= 0,
+	duplicateRegistration	= 263
 }
 
 local REASON = {
-	other		= 1,
-	parseError	= 2,
-	protocolError	= 3,
-	timeouts	= 4,
-	shutdown	= 5,
-	byManager	= 6
+	other			= 1,
+	parseError		= 2,
+	protocolError		= 3,
+	timeouts		= 4,
+	shutdown		= 5,
+	byManager		= 6
 }
 
 local val = { enc = {}, dec = {} }
@@ -79,8 +82,8 @@ function val.dec.objectid (pkt)
 	return pkt:sub(1 + 4 * len), v, include
 end
 
-function val.enc.searchrange (s, e, i)
-	return val.objectid(s, i) .. val.objectid(e)
+function val.enc.searchrange (t)
+	return val.objectid(t.start, t.include) .. val.objectid(t["end"])
 end
 
 function val.dec.searchrange (pkt)
@@ -97,15 +100,16 @@ function val.dec.octetstring (pkt)
 	return pkt:sub(5 + len + (4 - len % 4)), pkt:sub(5, len)
 end
 
-function val.enc.varbind (vtype, name, data)
-	if vtype == VTYPE.integer then
-		data = struct.pack(">I", data or 0)
-	elseif vtype == VTYPE.octetstring then
-		data = val.enc.octetstring(data)
+function val.enc.varbind (t)
+	local data
+	if t.type == VTYPE.integer then
+		data = struct.pack(">I", t.data or 0)
+	elseif t.type == VTYPE.octetstring then
+		data = val.enc.octetstring(t.data)
 	else
 		error("nyi")
 	end
-	return struct.pack(">H", vtype) .. "\0\0" .. val.enc.objectid(name) .. data
+	return struct.pack(">H", t.type) .. "\0\0" .. val.enc.objectid(t.name) .. data
 end
 
 function val.dec.varbind (pkt)
@@ -120,21 +124,23 @@ function val.dec.varbind (pkt)
 		pkt = pkt:sub(5)
 	elseif vtype == VTYPE.octetstring then
 		pkt, data = val.dec.octetstring(pkt)
+	elseif vtype == VTYPE.objectid then
+		pkt, data = val.dec.objectid(pkt)
 	else
-		error("nyi")
+		error("nyi " .. tostring(vtype))
 	end
-	return pkt, vtype, name, data
+	return pkt, { ["type"] = vtype, name = name, data = data }
 end
 
 local pdu = { enc = {}, dec = {} }
 
 -- https://datatracker.ietf.org/doc/html/rfc2741#section-6.1
-pdu.enc_hdr = function (self, t)
+pdu.enc_hdr = function (sessionID, t)
 	local flags = bit32.bor(t.flags and t.flags or 0x00, FLAGS.NETWORK_BYTE_ORDER)
-	return struct.pack(">BBBBIIII", 1, t.type, flags, 0, self._sessionID, 0, 0, t.payload:len()) .. t.payload
+	return struct.pack(">BBBBIIII", 1, t.type, flags, 0, sessionID, 0, 0, t.payload:len()) .. t.payload
 end
 
-pdu.dec_hdr = function (self, pkt)
+pdu.dec_hdr = function (pkt)
 	local version, ptype, flags, reserved, sessionID, transactionID, packetID, payload_length = struct.unpack(">BBBBIIII", pkt)
 	return {
 		version		= version,
@@ -148,42 +154,53 @@ pdu.dec_hdr = function (self, pkt)
 end
 
 -- https://datatracker.ietf.org/doc/html/rfc2741#section-6.2.1
-pdu.enc[TYPE.open] = function (self, t)
-	local deadtime = t.deadtime or DEADTIME
+pdu.enc[PTYPE.open] = function (sessionID, t)
+	local deadtime = t.deadtime or 0
 	local payload = struct.pack(">B", deadtime) .. "\0\0\0" .. val.enc.objectid({}) .. val.enc.octetstring(t.name)
-	return pdu.enc_hdr(self, {["type"]=TYPE.open, payload=payload})
+	return pdu.enc_hdr(sessionID, {["type"]=PTYPE.open, payload=payload})
 end
 
-pdu.enc[TYPE.close] = function (self, t)
+pdu.enc[PTYPE.close] = function (sessionID, t)
 	t = t or {}
 	local reason = t.reason or REASON.other
 	local payload = struct.pack(">B", reason) .. "\0\0\0"
-	return pdu.enc_hdr(self, {["type"]=TYPE.close, payload=payload})
+	return pdu.enc_hdr(sessionID, {["type"]=PTYPE.close, payload=payload})
 end
 
-pdu.dec[TYPE.close] = function (self, res, pkt)
+pdu.dec[PTYPE.close] = function (res, pkt)
 	local reason = struct.unpack(">B", pkt)
 	res.reason = reason
 	return res
 end
 
-pdu.enc[TYPE.indexAllocate] = function (self, t)
-	local payload = ""
-	for i, v in ipairs(t) do
-		payload = payload .. val.enc.varbind(v.type, v.name)
+pdu.enc[PTYPE.register] = function (sessionID, t)
+	local timeout = t.timeout or 0
+	local priority = t.priority or 127
+	local range_subid = t.range_subid or 0
+	local payload = struct.pack(">BBBB", timeout, priority, range_subid, 0) .. val.enc.objectid(t.subtree)
+	if range_subid > 0 then
+		payload = payload .. struct.pack(">I", t.upper_bound)
 	end
-	return pdu.enc_hdr(self, {["type"]=TYPE.indexAllocate, payload=payload, flags=t.flags})
+	return pdu.enc_hdr(sessionID, {["type"]=PTYPE.register, payload=payload})
 end
 
-pdu.enc[TYPE.indexDeallocate] = function (self, t)
+pdu.enc[PTYPE.indexAllocate] = function (sessionID, t)
 	local payload = ""
-	for i, v in ipairs(t) do
-		payload = payload .. val.enc.varbind(v.type, v.name, v.data)
+	for i, v in ipairs(t.varbind) do
+		payload = payload .. val.enc.varbind(v)
 	end
-	return pdu.enc_hdr(self, {["type"]=TYPE.indexDeallocate, payload=payload, flags=t.flags})
+	return pdu.enc_hdr(sessionID, {["type"]=PTYPE.indexAllocate, payload=payload, flags=t.flags})
 end
 
-pdu.dec[TYPE.response] = function (self, res, pkt)
+pdu.enc[PTYPE.indexDeallocate] = function (sessionID, t)
+	local payload = ""
+	for i, v in ipairs(t.varbind) do
+		payload = payload .. val.enc.varbind(v)
+	end
+	return pdu.enc_hdr(sessionID, {["type"]=PTYPE.indexDeallocate, payload=payload, flags=t.flags})
+end
+
+pdu.dec[PTYPE.response] = function (self, res, pkt)
 	local sysUpTime, perror, index = struct.unpack(">IHH", pkt)
 	res.sysUpTime = sysUpTime
 	res.error = perror
@@ -192,15 +209,15 @@ pdu.dec[TYPE.response] = function (self, res, pkt)
 	res.varbind = {}
 	pkt = pkt:sub(9)
 	while pkt:len() > 0 do
-		local vtype, name, data
-		pkt, vtype, name, data = val.dec.varbind(pkt)
-		table.insert(res.varbind, { ["type"] = vtype, name = name, data = data })
+		local varbind
+		pkt, varbind = val.dec.varbind(pkt)
+		table.insert(res.varbind, varbind)
 	end
 
 	return res
 end
 
-local M = { type = VTYPE, flags = FLAGS }
+local M = { type = VTYPE, flags = FLAGS, error = ERROR }
 
 function M:session (t)
 	t = t or {}
@@ -210,10 +227,9 @@ function M:session (t)
 
 	t.name = t.name or "Lua AgentX"
 	t.path = t.path or "/var/agentx/master"
-	t.deadtime = t.deadtime or DEADTIME
+	t.deadtime = 0
 
 	self._sessionID = 0
-	self._indexes = {}
 
 	self.fd = assert(socket.socket(socket.AF_UNIX, socket.SOCK_STREAM, 0))
 	local ok, err, e = socket.connect(self.fd, { family=socket.AF_UNIX, path=t.path })
@@ -222,7 +238,7 @@ function M:session (t)
 		return nil, err
 	end
 
-	M:send(pdu.enc[TYPE.open](self, t))
+	M:send(pdu.enc[PTYPE.open](self._sessionID, t))
 	local r = poll.rpoll(self.fd, 100)
 	if r == 0 then
 		M:close()
@@ -241,11 +257,8 @@ function M:session (t)
 end
 
 function M:close ()
-	for i=#self._indexes,1,-1 do
-		M:index_deallocate(self._indexes[i])
-	end
 	if self._sessionID ~= nil then
-		M:send(pdu.enc[TYPE.close](self))
+		M:send(pdu.enc[PTYPE.close](self._sessionID))
 		local res = M:recv()
 		if res.error ~= ERROR.noAgentXError then
 			return false, "AgentX master returned error code " .. tostring(res.error)
@@ -259,39 +272,25 @@ function M:close ()
 	return true
 end
 
+function M:register (t)
+	M:send(pdu.enc[PTYPE.register](self._sessionID, t))
+	return M:recv()
+end
+
 function M:index_allocate (t)
 	if t.name then
-		t = {t}
+		t = { flags = t.flags, varbind = { { ["type"] = t.type, name = t.name, data = t.data } } }
 	end
-	M:send(pdu.enc[TYPE.indexAllocate](self, t))
-	local res = M:recv()
-	if res.error ~= ERROR.noAgentXError then
-		return nil, "AgentX master returned error code " .. tostring(res.error)
-	end
-	for i, v in ipairs(res.varbind) do
-		table.insert(self._indexes, v)
-	end
-	return res
+	M:send(pdu.enc[PTYPE.indexAllocate](self._sessionID, t))
+	return M:recv()
 end
 
 function M:index_deallocate (t)
 	if t.name then
-		t = {t}
+		t = { flags = t.flags, varbind = { { ["type"] = t.type, name = t.name, data = t.data } } }
 	end
-	M:send(pdu.enc[TYPE.indexDeallocate](self, t))
-	local res = M:recv()
-	if res.error ~= ERROR.noAgentXError then
-		return nil, "AgentX master returned error code " .. tostring(res.error)
-	end
-	for ti, tt in ipairs(t) do
-		for i, v in ipairs(self._indexes) do
-			if table.concat(tt.name, ".") == table.concat(v.name, ".") and tt.data == v.data then
-				table.remove(self._indexes, i)
-				break
-			end
-		end
-	end
-	return res
+	M:send(pdu.enc[PTYPE.indexDeallocate](self._sessionID, t))
+	return M:recv()
 end
 
 function M:send (msg)
@@ -302,14 +301,19 @@ function M:recv ()
 	-- stream socket so keep pulling...
 	local hdr = ""
 	while true do
-		hdr = hdr .. socket.recv(self.fd, HDRSIZE - hdr:len())
+		local buf = socket.recv(self.fd, HDRSIZE - hdr:len())
+		if buf:len() == 0 then
+			self.fd = nil
+			return nil, "connection closed"
+		end
+		hdr = hdr .. buf
 		if hdr:len() == 20 then break end
 	end
 
-	local res = { _hdr = pdu.dec_hdr(self, hdr) }
+	local res = { _hdr = pdu.dec_hdr(hdr) }
 
 	-- for now this is all we support
-	assert(res._hdr.type == TYPE.response)
+	assert(res._hdr.type == PTYPE.response)
 
 	local payload = ""
 	while true do
@@ -317,12 +321,7 @@ function M:recv ()
 		if payload:len() == res._hdr.payload_length then break end
 	end
 
-	local status, res = pcall(function() return pdu.dec[res._hdr.type](self, res, payload) end)
-	if not status then
-		error("nyi decode for " .. tostring(res._hdr.type))
-	end
-
-	return res
+	return pdu.dec[res._hdr.type](self, res, payload)
 end
 
 return M
--- snmp-agentx.lua
+++ snmp-agentx.lua
@@ -8,7 +8,7 @@ local dir = arg[0]:match("^(.-/?)[^/]+.lua$")
 
 local status, agentx = pcall(function () return require "agentx" end)
 if not status then
-	struct = assert(loadfile(dir .. "agentx.lua"))()
+	agentx = assert(loadfile(dir .. "agentx.lua"))()
 end
 local status, ebm = pcall(function () return require "ebm" end)
 if not status then
@@ -20,12 +20,34 @@ if #arg < 2 then
 	os.exit(1)
 end
 
-local iftable_ifentry = {1,3,6,1,2,1,2,2,1}
+local iftable_ifindex = {1,3,6,1,2,1,2,2,1,1}
 local session = agentx:session({name="EBM"})
-local res, err = session:index_allocate({type=agentx.type.integer, name=iftable_ifentry, flags=agentx.flags.NEW_INDEX})
-if err then
-	error(err)
+local ifindex
+while not ifindex do
+	local res
+
+	res = session:index_allocate({["type"]=agentx.type.integer, name=iftable_ifindex, flags=agentx.flags.NEW_INDEX})
+	if res.error ~= agentx.error.noAgentXError then
+		error(res.error)
+	end
+
+	ifindex = res.varbind[1].data
+
+	local iftable = {unpack(iftable_ifindex), ifindex}
+	res = session:register({range_subid=#iftable - 1, subtree=iftable, upper_bound=22})
+	if res.error == agentx.error.noAgentXError then
+		break
+	elseif res.err == agentx.error.duplicateRegistration then
+		res = session:index_deallocate({["type"]=agentx.type.integer, name=iftable_ifindex, data=ifindex})
+		if res.error ~= agentx.error.noAgentXError then
+			error(res.error)
+		end
+		ifindex = nil
+	else
+		error(res.error)
+	end
 end
+-- print(ifindex)
 session:close()
 
 -- local session = ebm:session({iface=arg[1], addr=arg[2]})
